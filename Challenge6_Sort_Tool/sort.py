80658e0e7678554dbd954724695d1e8a12510e83
omr50 <omrab4@gmail.com>
2024-10-13 01:03:08 -0400

    Radix commentary

--- Challenge6_Sort_Tool/sort.py
+++ Challenge6_Sort_Tool/sort.py
@@ -70,6 +70,29 @@ def heapsort(array):
 
     return output
 
+# this sort involves counting repeatedly for as many digits as the longest item
+# it does a repetetive bucket sort, starting from the smallest digit to the largest
+# digit. On subsequent iterations, the sort on the current digit will still keep any 
+# items that were in the same group for the previous and current iteration in the same
+# order. So an item in the nth group and the other in the n + 1th group in the previous
+# iteration but in the current one they have the same digit, then we will still order them
+# by the one that was in the nth group in the last iteration then the n + 1th item.
+
+# 159, and 139 for example. In the first sort they both belong to the 9 group [159, 139]
+# in the second iteration we see that one is in the 3 group [139], the other is in the 5 [159]
+# next they will both go into the 1 group, but since we use the previous groupings and we start
+# from index 0 to 9, we will come across 139 first, so it is placed in the 1 index [139], then we
+# will find nothing in the 4 index, then the 5 index has 159 so we take it next. But see that the 
+# previous groupings influence our current sorting. So then 159 is added [139, 159], so we have a fully
+# sorted item.
+
+# what happens when we have only integers, we can do 0-9
+# if we have only lower case letters or only upper case 0-25
+# if we have both then 0 - 51
+# if ascii, then 0 - 127 i think. if its a file (best to play it safe and do ascii)
+def radixsort(array):
+
+    return 0   
 # output = (merge_sort(words_array, 0, len(words_array)-1))
 output = heapsort(words_array)
 output_str = "\n".join(output)
